a40cabe845e27c0e1c5dc2bcd203ffabc7275bf2
angelsainz10 <angelsainzhrz@gmail.com>
2022-11-17 17:29:32 -0600

    share_preferences

--- lib/src/services/login_service.dart
+++ lib/src/services/login_service.dart
@@ -1,47 +1,21 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:test/src/constants/constans.dart';
-import 'package:test/src/pages/home.dart';
+import 'package:test/src/utils/globals.dart';
+import 'package:test/src/utils/shared_preferences.dart';
 
 class LoginService {
-  static Future login(context, username, password) async {
-    var map = <String, dynamic>{};
-    map['username'] = username;
-    map['password'] = password;
-    await http
-        .post(
-      Uri.parse("${Constants.APIURL}/api/login/"),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: json.encode(map),
-    )
-        .then((response) async {
-      if (response.statusCode == 200) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const Home(),
-          ),
-        );
-      } else {
-        print(json.decode(response.body)['error']);
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(json.decode(response.body)['error'].toString()),
-          backgroundColor: Colors.red,
-        ));
-      }
-    }).catchError((error) {
-      if (error.runtimeType.toString() == 'SocketException') {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Verifica tu conexión.'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    });
+  static Future<bool> login(context, String user, String pass) {
+    bool logged = false;
+    String prefPass = SharedPreferencesGlobal.prefs.getString(user) ?? '';
+    if (pass == prefPass) {
+      logged = true;
+      Globals.user = user;
+      Globals.isAdmin = (user == "admin");
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("Credenciales incorrectas"),
+        backgroundColor: Colors.red,
+      ));
+    }
+    return Future.value(logged);
   }
 }
